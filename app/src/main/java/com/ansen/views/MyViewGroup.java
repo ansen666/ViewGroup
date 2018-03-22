42ca003c424091e9e24ea8e3a0a6a7d8c83ad4f3
anhui <privateworkemail@163.com>
2018-03-23 02:37:28 +0800

    更新

--- app/src/main/java/com/ansen/views/MyViewGroup.java
+++ app/src/main/java/com/ansen/views/MyViewGroup.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ansen.viewgroup.R;
 
@@ -24,79 +25,78 @@ public class MyViewGroup extends ViewGroup{
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int count=getChildCount();
-        Log.i("ansen","count:"+count);
-        int left=0;
-
-        for(int i=0;i<count;i++){
-            View child=getChildAt(i);
-
-            LayoutParams lp= (LayoutParams) child.getLayoutParams();
-
-            int childWidth=child.getMeasuredWidth();
-            int childHeight=child.getMeasuredHeight();
-
-            if(lp.position==LayoutParams.POSITION_RIGHT){//当前子View显示在ViewGroup右边
-                child.layout(getWidth()-childWidth,0,getWidth(),childHeight);
-            }else if(lp.position==LayoutParams.POSITION_BOTTOM){////当前子View显示在ViewGroup底部
-                child.layout(left+lp.leftMargin,getHeight()-childHeight,left+childWidth+lp.leftMargin,getHeight());
-            }else{
-                child.layout(left+lp.leftMargin,0,left+childWidth+lp.leftMargin,child.getMeasuredHeight());
-            }
-
-            Log.i("ansen","left:"+left+" top:"+0+" right:"+(left+childWidth)+" bottom:"+childHeight);
-
-            left+=childWidth+lp.leftMargin+lp.rightMargin;
-        }
-    }
-
-    @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        /**
-         * 获得此ViewGroup上级容器为其推荐的宽和高，以及计算模式
-         */
+        //获得此ViewGroup计算模式
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
+        //获取ViewGroup宽高
         int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
         int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
 
-        measureChildren(widthMeasureSpec,heightMeasureSpec);
+        measureChildren(widthMeasureSpec,heightMeasureSpec);//测量所有子View的宽高
 
         if(getChildCount()<=0){//如果没有子View 当前ViewGroup的宽高直接设置为0
             setMeasuredDimension(0,0);
-        }if(heightMode==MeasureSpec.AT_MOST&&widthMode==MeasureSpec.AT_MOST){//宽或者高是wrap_content
+        }if(heightMode==MeasureSpec.AT_MOST&&widthMode==MeasureSpec.AT_MOST){//宽跟高是wrap_content
             int measuredWidth=0;//测量宽度
             int maxMeasuredHeigh=0;//测量高度子View最大的高度
             for(int i=0;i<getChildCount();i++){
                 View child=getChildAt(i);
-                LayoutParams lp= (LayoutParams) child.getLayoutParams();
-                measuredWidth+=child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
 
-                if(child.getMeasuredHeight()>maxMeasuredHeigh){
+
+                MyLayoutParams lp= (MyLayoutParams) child.getLayoutParams();
+                measuredWidth+=child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;//加上左右边距
+
+                if(child.getMeasuredHeight()>maxMeasuredHeigh){//如果当前的View大于之后View的高度
                     maxMeasuredHeigh=child.getMeasuredHeight();
                 }
             }
             setMeasuredDimension(measuredWidth,maxMeasuredHeigh);
         }else{
-            Log.i("ansen","测量宽度:"+sizeWidth+" 测量高度:"+sizeHeight);
             setMeasuredDimension(sizeWidth,sizeHeight);
         }
     }
-    
+
     @Override
-    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs){
-        return new LayoutParams(getContext(), attrs);
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.i("ansen","onLayout");
+        int count=getChildCount();//获取子View数量
+        int left=0;//上一个View到ViewGroup的宽度距离
+
+        for(int i=0;i<count;i++){
+            View child=getChildAt(i);
+
+            MyLayoutParams lp= (MyLayoutParams) child.getLayoutParams();
+
+            int childWidth=child.getMeasuredWidth();//获取子View宽度
+            int childHeight=child.getMeasuredHeight();//获取子View高度
+
+            if(lp.position==MyLayoutParams.POSITION_RIGHT){//当前子View显示在ViewGroup右边
+                child.layout(getWidth()-childWidth,0,getWidth(),childHeight);//设置摆放位置
+            }else if(lp.position==MyLayoutParams.POSITION_BOTTOM){////当前子View显示在ViewGroup底部
+                child.layout(left+lp.leftMargin,getHeight()-childHeight,left+childWidth+lp.leftMargin,getHeight());
+            }else{//没有设置位置的View
+                child.layout(left+lp.leftMargin,0,left+childWidth+lp.leftMargin,child.getMeasuredHeight());
+            }
+
+            Log.i("ansen","left:"+left+" top:"+0+" right:"+(left+childWidth)+" bottom:"+childHeight);
+            left+=childWidth+lp.leftMargin+lp.rightMargin;
+        }
     }
 
-    public static class LayoutParams extends MarginLayoutParams {
-        public static int POSITION_RIGHT = 1;
-        public static int POSITION_BOTTOM = 2;
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs){
+        return new MyLayoutParams(getContext(), attrs);
+    }
 
-        public int position = -1;
+    public static class MyLayoutParams extends MarginLayoutParams {
+        public static int POSITION_RIGHT = 1;//右边
+        public static int POSITION_BOTTOM = 2;//底部
 
-        public LayoutParams(Context c, AttributeSet attrs) {
+        public int position = -1;//
+
+        public MyLayoutParams(Context c, AttributeSet attrs) {
             super(c, attrs);
 
             TypedArray a = c.obtainStyledAttributes(attrs, R.styleable.CustomLayoutLP);
@@ -104,11 +104,11 @@ public class MyViewGroup extends ViewGroup{
             a.recycle();
         }
 
-        public LayoutParams(int width, int height) {
+        public MyLayoutParams(int width, int height) {
             super(width, height);
         }
 
-        public LayoutParams(ViewGroup.LayoutParams source) {
+        public MyLayoutParams(ViewGroup.LayoutParams source) {
             super(source);
         }
     }
